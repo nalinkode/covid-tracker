f263e8f5eff76ea9ec3073885de2d27a5d6833ad
nalinkode <nalinkode35@gmail.com>
2020-09-06 10:18:34 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, NavBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
