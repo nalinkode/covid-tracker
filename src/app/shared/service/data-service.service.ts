869cccdb19b793dff049aa8bc907a4e75de1fdbb
nalinkode <nalinkode35@gmail.com>
2020-09-06 11:55:31 +0530

    2 files modified

--- src/app/shared/service/data-service.service.ts
+++ src/app/shared/service/data-service.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map } from 'rxjs/operators';
 
 @Injectable({
    providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor(private http : ) { }
+  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-05-2020.csv'
+
+  constructor(private http : HttpClient ) { }
+
+  getGlobalData(){
+  return this.http.get(this.globalDataUrl, {responseType :  'text'}).pipe(map(result=>{
+         
+      let rows = result.split('\n');
+      console.log(rows);
+
+
+  }))
+  }
 
 }
